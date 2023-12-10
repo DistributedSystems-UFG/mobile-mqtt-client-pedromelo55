8cc707eb0e8cb9ae256e66a083930f2b7a44bcf7
Pedro Melo <pedromelo55@gmail.com>
2023-12-10 15:08:02 -0300

    Aplicativo pronto

--- app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
+++ app/src/main/java/com/example/basicandroidmqttclient/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.basicandroidmqttclient;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
@@ -14,14 +17,16 @@ import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.basicandroidmqttclient.MESSAGE";
-    public static final String brokerURI = "3.223.10.115";
+    public static final String brokerURI = "34.234.31.226";
 
     Activity thisActivity;
-    TextView subMsgTextView;
+    ListView listView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +34,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         thisActivity = this;
-        subMsgTextView = (TextView) findViewById(R.id.editTextMultiLineSubMsg);
+        listView = findViewById(R.id.listViewSubMsg);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+        listView.setAdapter(adapter);
     }
 
     /** Called when the user taps the Send button */
     public void publishMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText topicName = (EditText) findViewById(R.id.editTextTopicName);
-        EditText value = (EditText) findViewById(R.id.editTextValue);
+        EditText topicName = findViewById(R.id.editTextTopicName);
+        EditText value = findViewById(R.id.editTextValue);
 
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
@@ -57,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendSubscription(View view) {
-        EditText topicName = (EditText) findViewById(R.id.editTextTopicNameSub);
+        EditText topicName = findViewById(R.id.editTextTopicNameSub);
 
         Mqtt5BlockingClient client = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
@@ -71,15 +78,11 @@ public class MainActivity extends AppCompatActivity {
                 .topicFilter(topicName.getText().toString())
                 .qos(MqttQos.AT_LEAST_ONCE)
                 .callback(msg -> {
-                    thisActivity.runOnUiThread(new Runnable() {
-                        public void run() {
-                            subMsgTextView.setText(new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8));
-                        }
+                    thisActivity.runOnUiThread(() -> {
+                        String message = new String(msg.getPayloadAsBytes(), StandardCharsets.UTF_8);
+                        adapter.insert(message, 0);
                     });
                 })
                 .send();
     }
-
-
-
 }
